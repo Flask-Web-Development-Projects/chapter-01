e343094a25f515d154f54210778569a0ca719992
nhuntwalker <nhuntwalker@Gmail.com>
2019-04-18 07:20:01 -0700

    Generated the react application. Need to point the Flask server there

---
+++ client/build/precache-manifest.6d35312cda22dd0d834524c5ec9bcc04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "707061f07f85813529f9",
+    "url": "/static/css/main.cf1b4a73.chunk.css"
+  },
+  {
+    "revision": "707061f07f85813529f9",
+    "url": "/static/js/main.1671c159.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "10534ad094e56cc863c6",
+    "url": "/static/js/2.4b44abd2.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b5912b2ed173fa3e10c83f3c1595e62e",
+    "url": "/index.html"
+  }
+];
