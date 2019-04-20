c28aed4cbdc64d7d24b132b5e03fc5aae61777c2
nhuntwalker <nhuntwalker@Gmail.com>
2019-04-20 13:26:18 -0700

    Broke out the task into its own component and created a space for action buttons, starting with the delete button

--- client/src/components/TaskList/index.tsx
+++ client/src/components/TaskList/index.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import {Task} from '../types';
 
+
+interface TaskProps {
+    task: Task;
+};
+
 interface Props {
     tasks: Task[];
-}
+};
+
+const Task = ({ task }: TaskProps) => {
+    return <div key={task._id}>
+        <div className="task-buttons">
+            <button>Delete</button>
+        </div>
+        <div className="task-body">
+            {task.body}
+        </div>
+    </div>;
+};
 
 export const TaskList = ({ tasks }: Props) => {
     return <div>
-        {tasks.map(task => <div key={task._id}>{task.body}</div>)}
+        {tasks.map(task => <Task task={task} />)}
     </div>;
-}
+};
