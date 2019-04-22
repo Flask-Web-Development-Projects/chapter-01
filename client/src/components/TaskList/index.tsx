dd0c2ed3b0a07785c4c9258de36a4b5d5dc808d4
nhuntwalker <nhuntwalker@Gmail.com>
2019-04-21 22:21:46 -0700

    Put the TaskItem component into its own file, as its about to get its own set of components for toggling between "being edited" and "being shown"

--- client/src/components/TaskList/index.tsx
+++ client/src/components/TaskList/index.tsx
@@ -1,38 +1,12 @@
 import React, { useState } from 'react';
+import { TaskItem } from '../TaskItem';
 import {Task} from '../types';
 
-
-interface TaskProps {
-    task: Task;
-    deleteTask: (taskId: string) => void;
-    toBeEdited: (taskId: string) => void;
-    completeTask: (task: Task) => void;
-    isEditing: string;
-};
 
 interface ListProps {
     tasks: Task[];
     deleteTask: (taskId: string) => void;
     completeTask: (task: Task) => void;
-};
-
-const TaskItem = ({ task, deleteTask, completeTask, toBeEdited, isEditing }: TaskProps) => {
-    return <div key={task._id}>
-        <div className="task-buttons">
-            <button onClick={() => deleteTask(task._id)}>
-                Delete
-            </button>
-            <button onClick={() => completeTask(task)}>
-                Complete
-            </button>
-            <button onClick={() => toBeEdited(task._id)}>
-                Edit
-            </button>
-        </div>
-        <div className="task-body">
-            {task.body}
-        </div>
-    </div>;
 };
 
 export const TaskList = ({ tasks, deleteTask, completeTask }: ListProps) => {
