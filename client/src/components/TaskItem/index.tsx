64689a82a8ef2fddc386d1b46ba00b9b7b44ed4f
nhuntwalker <nhuntwalker@Gmail.com>
2019-04-21 22:24:17 -0700

    Broke the task buttons out into their own component

--- client/src/components/TaskItem/index.tsx
+++ client/src/components/TaskItem/index.tsx
@@ -9,19 +9,32 @@ interface TaskProps {
     isEditing: string;
 };
 
+interface ButtonProps {
+    task: Task;
+    deleteTask: (taskId: string) => void;
+    toBeEdited: (taskId: string) => void;
+    completeTask: (task: Task) => void;
+}
+
+const TaskButtons = ({ task, deleteTask, completeTask, toBeEdited }: ButtonProps) => {
+    return <div className="task-buttons">
+        <button onClick={() => deleteTask(task._id)}>
+            Delete
+            </button>
+        <button onClick={() => completeTask(task)}>
+            Complete
+            </button>
+        <button onClick={() => toBeEdited(task._id)}>
+            Edit
+            </button>
+    </div>;
+};
+
 export const TaskItem = ({ task, deleteTask, completeTask, toBeEdited, isEditing }: TaskProps) => {
     return <div key={task._id}>
-        <div className="task-buttons">
-            <button onClick={() => deleteTask(task._id)}>
-                Delete
-            </button>
-            <button onClick={() => completeTask(task)}>
-                Complete
-            </button>
-            <button onClick={() => toBeEdited(task._id)}>
-                Edit
-            </button>
-        </div>
+       <TaskButtons
+            {...{ task, deleteTask, completeTask, toBeEdited }}
+       />
         <div className="task-body">
             {task.body}
         </div>
