ba8ec699d8451176adec188d66aff35599491ce8
nhuntwalker <nhuntwalker@Gmail.com>
2019-04-21 22:17:38 -0700

    Added props to the TaskItem and TaskList for setting which task is about to be edited

--- client/src/components/TaskList/index.tsx
+++ client/src/components/TaskList/index.tsx
@@ -1,20 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {Task} from '../types';
 
 
 interface TaskProps {
     task: Task;
-    deleteTask: (taskId: string) => {};
-    completeTask: (task: Task) => {};
+    deleteTask: (taskId: string) => void;
+    toBeEdited: (taskId: string) => void;
+    completeTask: (task: Task) => void;
+    isEditing: string;
 };
 
 interface ListProps {
     tasks: Task[];
-    deleteTask: (taskId: string) => {};
-    completeTask: (task: Task) => {};
+    deleteTask: (taskId: string) => void;
+    completeTask: (task: Task) => void;
 };
 
-const TaskItem = ({ task, deleteTask, completeTask }: TaskProps) => {
+const TaskItem = ({ task, deleteTask, completeTask, toBeEdited, isEditing }: TaskProps) => {
     return <div key={task._id}>
         <div className="task-buttons">
             <button onClick={() => deleteTask(task._id)}>
@@ -22,6 +24,9 @@ const TaskItem = ({ task, deleteTask, completeTask }: TaskProps) => {
             </button>
             <button onClick={() => completeTask(task)}>
                 Complete
+            </button>
+            <button onClick={() => toBeEdited(task._id)}>
+                Edit
             </button>
         </div>
         <div className="task-body">
@@ -31,7 +36,18 @@ const TaskItem = ({ task, deleteTask, completeTask }: TaskProps) => {
 };
 
 export const TaskList = ({ tasks, deleteTask, completeTask }: ListProps) => {
+    const [isEditing, setEditedTask] = useState('');
+    const toBeEdited = (taskId: string) => {
+        setEditedTask(taskId);
+    };
+    let taskProps = { deleteTask, completeTask, toBeEdited, isEditing };
     return <div>
-        {tasks.map(task => <TaskItem {...{task, deleteTask, completeTask}} />)}
+        {tasks
+            .map(task => 
+                <TaskItem 
+                    {...{task, ...taskProps}}
+                />
+            )
+        }
     </div>;
 };
